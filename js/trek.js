d15151572bb88db7a2905cfbcd5a30d1301fe972
pranalishinde06 <shindepranali801@gmail.com>
2026-03-07 22:55:08 +0530

    navbar added

--- js/trek.js
+++ js/trek.js
@@ -1,3 +1,8 @@
-<script>
-    console.log("Hello")
-</script>
+function toggleMenu(){
+
+let menu = document.getElementById("navMenu");
+let auth = document.querySelector(".auth-links-containar");
+
+menu.classList.toggle("active");
+auth.classList.toggle("active");
+}
